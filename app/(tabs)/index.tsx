bf63426a89bc40f83ec3fec7398e766847ad8c3c
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-01-13 12:46:06 -0300

    começa home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
+import { height, width } from "@/constants/Dimensions";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {BarChart, Plus} from "react-native-feather"
 
-export default function HomeScreen(){
-    return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                Home
-            </Text>
-        </View>
-    )
+export default function HomeScreen() {
+  return (
+    <View style={style.container}>
+      <View style={{display: "flex", width: "90%", alignItems: "flex-start", }}>
+        <Text style={{fontWeight:"600", fontSize: width * 0.05, color: "white"}}>Hello Jane Doe!</Text>
+      </View>
+      <View style={{backgroundColor:"#7a62dd", width: "90%", borderRadius: 10, height: "20%", justifyContent: "center", display:"flex"}}>
+        <BarChart color={"white"}/>
+        <Text style={{color: "white", fontSize: width * 0.07}}>$1.697,25</Text>
+        <Plus width={30} height={30}/>
+      </View>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#17194a",
+  },
+});
--- constants/Dimensions.tsx
+++ constants/Dimensions.tsx
@@ -2,3 +2,5 @@ import { Dimensions } from "react-native";
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
+
+export {width, height};
