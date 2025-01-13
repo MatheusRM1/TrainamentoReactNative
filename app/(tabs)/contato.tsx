6173773c0b7a08f7666fd909d8908168ac22de96
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-01-13 11:38:49 -0300

    configura projeto inicial

---
+++ app/(tabs)/contato.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function ContatoScreen(){
+    return(
+        <View>
+            <Text>
+                Contato
+            </Text>
+        </View>
+    )
+}
